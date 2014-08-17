208b9cb98dc926b22d8e44fa63580c3b7a071acf
olive <olivine.dogue@gmail.com>
2014-08-17 13:11:20 -0400

    Added more colors

--- src/Antiqua/Graphics/Color.hs
+++ src/Antiqua/Graphics/Color.hs
@@ -13,6 +13,15 @@ black = rgb 0 0 0
 white :: Color
 white = rgb 255 255 255
 
+grey :: Color
+grey = rgb 128 128 128
+
+darkGrey :: Color
+darkGrey = rgb 64 64 64
+
+brown :: Color
+brown = rgb 150 75 0
+
 red :: Color
 red = rgb 255 0 0
 
